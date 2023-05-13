b3866b7dca51a5a7d134a1ca694922eae013a68f
alon55 <alonzager5@gmail.com>
2023-05-13 18:52:02 +0300

    blue - 2 different books - 5% discount

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [cleanCode, setCleanCode] = useState(0)
+  const [cleanCoder, setCleanCoder] = useState(0)
+  const [cleanArchitecture, setCleanArchitecture] = useState(0)
+  const [tdd, setTdd] = useState(0)
+  const [legacyCode, setLegacyCode] = useState(0)
+  const [totalPrice, setTotalPrice] = useState(0)
+  let bookArr = []
+
+  useEffect(() => {
+    bookArr = [
+      ...Array(cleanCode).fill('cleanCode'),
+      ...Array(cleanCoder).fill('cleanCoder'),
+      ...Array(cleanArchitecture).fill('cleanArchitecture'),
+      ...Array(tdd).fill('tdd'),
+      ...Array(legacyCode).fill('legacyCode')
+    ]
+  }, [cleanCode, cleanCoder, cleanArchitecture, tdd, legacyCode])
+
+  const calculateBooksPrice = (bookArr) => {
+    let totalPrice = 0
+
+    if (bookArr.length === 2) { totalPrice += 2 * 50 * 0.95 }
+
+    setCleanCode(0)
+    setCleanCoder(0)
+    setCleanArchitecture(0)
+    setTdd(0)
+    setLegacyCode(0)
+    setTotalPrice(totalPrice)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="https://github.com/stephane-genicot/katas/raw/master/images/Kata_DevelopmentBooks_TDD.jpeg?raw=true" className="App-logo" alt="logo" />
+      <p>
+        Book price calculator - TDD
+      </p>
+      <div style={{ textAlign: 'start', padding: '10px 20vh' }}>
+        <label htmlFor="clean-code">Clean Code <input type="number" min={0} id="clean-code" value={cleanCode} onChange={e => setCleanCode(Number(e.target.value))}></input></label>
+        <label htmlFor="clean-coder">The Clean Coder <input type="number" min={0} id="clean-coder" value={cleanCoder} onChange={e => setCleanCoder(Number(e.target.value))}></input></label>
+        <label htmlFor="clean-architecture">Clean Architecture <input type="number" min={0} id="clean-architecture" value={cleanArchitecture} onChange={e => setCleanArchitecture(Number(e.target.value))}></input></label>
+        <label htmlFor="tdd">Test Driven Development <input type="number" id="tdd" min={0} value={tdd} onChange={e => setTdd(Number(e.target.value))}></input></label>
+        <label htmlFor="legacy-code">Legacy Code <input type="number" id="legacy-code" min={0} value={legacyCode} onChange={e => setLegacyCode(Number(e.target.value))}></input></label>
+      </div>
+      <br />
+      <button onClick={() => calculateBooksPrice(bookArr)}>Calculate price</button>
+      <h3>{totalPrice ? 'The total price is: ' + totalPrice : null}</h3>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Book price calculator - Tests", () => {
+
+  test("2 different books - 5% discount", async () => {
+    render(<App />)
+
+    const CleanCodeInput = screen.getByLabelText('Clean Code')
+    fireEvent.change(CleanCodeInput, { target: { value: 1 } })
+    const TheCleanCoderInput = screen.getByLabelText('The Clean Coder')
+    fireEvent.change(TheCleanCoderInput, { target: { value: 1 } })
+
+    const calculatePrice = screen.getByRole('button', { name: /Calculate price/i });
+    fireEvent.click(calculatePrice)
+
+    const totalPrice = screen.getByRole('heading', { level: 3 });
+    expect(totalPrice.innerHTML).toBe('The total price is: 95');
+  });
+
+
 });
